70688eaee31de28d6e931df0448c855f635398ce
Ivan Stefanov <50416533+vansky17@users.noreply.github.com>
2025-12-21 17:39:48 +0100

    Blackhole Initial Implementation

--- Source/Action_CPlus/Projectiles/VanProjectileBlackhole.cpp
+++ Source/Action_CPlus/Projectiles/VanProjectileBlackhole.cpp
@@ -2,15 +2,49 @@
 
 
 #include "VanProjectileBlackhole.h"
-
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "PhysicsEngine/RadialForceComponent.h"
 
 // Sets default values
 AVanProjectileBlackhole::AVanProjectileBlackhole()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	RadialForceComponent =
+		CreateDefaultSubobject<URadialForceComponent>(TEXT("RadialForceComponent"));
+	RadialForceComponent->SetupAttachment(SphereComponent);
+
+	RadialForceComponent->ForceStrength = -800000.0f;
+	RadialForceComponent->Radius = 1200.0f;
+	RadialForceComponent->bAutoActivate = true;
+	RadialForceComponent->bIgnoreOwningActor = true;
+
+	RadialForceComponent->RemoveObjectTypeToAffect(
+		UEngineTypes::ConvertToObjectType(ECC_Pawn));
+
+	SphereComponent->SetSphereRadius(20.0f);
+	SphereComponent->SetCollisionProfileName("BlackholeCore");
+
+	ProjectileMovementComponent->InitialSpeed = 500.0f;
+
+	InitialLifeSpan = 5.0f;
+}
+void AVanProjectileBlackhole::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	// Note: Make sure GenerateOverlapEvents is enabled on the cubes in the world
+	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &AVanProjectileBlackhole::OnSphereOverlappedActor);
+	
 }
 
+void AVanProjectileBlackhole::OnSphereOverlappedActor(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherComp->IsSimulatingPhysics())
+	{
+		OtherActor->Destroy();
+	}
+}
 // Called when the game starts or when spawned
 void AVanProjectileBlackhole::BeginPlay()
 {
--- Source/Action_CPlus/Projectiles/VanProjectileBlackhole.h
+++ Source/Action_CPlus/Projectiles/VanProjectileBlackhole.h
@@ -6,18 +6,29 @@
 #include "VanProjectile.h"
 #include "VanProjectileBlackhole.generated.h"
 
+class URadialForceComponent;
+
 UCLASS()
 class ACTION_CPLUS_API AVanProjectileBlackhole : public AVanProjectile
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this actor's properties
+	
+	virtual void PostInitializeComponents() override;
 	AVanProjectileBlackhole();
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
+	UPROPERTY(VisibleAnywhere, Category= "Components")
+	TObjectPtr<URadialForceComponent> RadialForceComponent;
+
+	UFUNCTION()
+	void OnSphereOverlappedActor(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+
 
 public:
 	// Called every frame
