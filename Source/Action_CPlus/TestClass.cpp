d454206d66704835b564aa1cdfa5ccb1d7551941
Ivan Stefanov <50416533+vansky17@users.noreply.github.com>
2025-12-29 20:23:10 +0100

    Test after restore

---
+++ Source/Action_CPlus/TestClass.cpp
@@ -0,0 +1,26 @@
+﻿// DELETE
+
+
+#include "TestClass.h"
+
+
+// Sets default values
+ATestClass::ATestClass()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ATestClass::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATestClass::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/Action_CPlus/TestClass.h
@@ -0,0 +1,25 @@
+﻿// DELETE
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TestClass.generated.h"
+
+UCLASS()
+class ACTION_CPLUS_API ATestClass : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ATestClass();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
